c7be3243feaa36ebafa420cc561efdbeaa945851
kingdreamerr <jackmvula458@gmail.com>
2023-01-16 09:54:49 +0200

    Add logic for the calculator

--- main.py
+++ main.py
@@ -20,3 +20,24 @@ operations = {
   "*": multiply,
   "/":divide
 }
+
+def calculate():
+  continue_claculating = False
+  num1 = float(input("What's the first number?: "))
+  
+  while not continue_claculating:
+    ops()
+    op = input("Pick an operation from the line above: ")
+    num2 = float(input("What's the next number?: "))
+
+    answer = operations[op](num1,num2)
+    print(f"{num1} {op} {num2} = {answer}")
+
+    yes = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start .: ")
+    if yes == 'y':
+     num1 = answer
+    else:
+      continue_claculating = True
+      calculate()
+
+calculate()
